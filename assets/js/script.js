63a41f7aec603176416a8fb946f8e180757208c8
Ademinh0 <madem1478@gmail.com>
2025-12-08 16:01:01 +0100

    initialisation portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const projets = [
+  {
+    id: 1,
+    titre: "Star wars",
+    lien:
+      "https://star-wars1-kappa.vercel.app/",
+  },
+  {
+    id: 2,
+    titre: "Culture&Co",
+    lien:
+      "https://www.figma.com/design/NGawnH0dal11uffqVP0tg4/Projet-Culture-Co-Adem?node-id=0-1",
+  },
+  {
+    id: 3,
+    titre: "Site Mariage", 
+    lien:
+      "https://photo-mariage-j3lh.vercel.app/",
+  },
+];
